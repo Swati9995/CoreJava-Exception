0b7ac4837c246edc15769d58297526e3f367d071
Swati9995 <136185887+Swati9995@users.noreply.github.com>
2023-06-11 11:36:57 +0530

    code commited by swati

---
+++ SteamApiExamples/src/com/collections/exps/CollectionExample1.java
@@ -0,0 +1,16 @@
+package com.collections.exps;
+
+public class CollectionExample1 {
+	
+	
+	public static void main(String args) {
+		
+		System.out.println("In main method");
+	}
+	
+public void hello() {
+	
+	System.out.println("In hello method");
+}
+
+}
